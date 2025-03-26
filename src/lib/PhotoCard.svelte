5eb6ded04213c374bcbdd5deb4e8d373c18b9992
Tsvetan Igov <ts.igov@outlook.com>
2025-03-26 23:40:33 +0200

    Project init

---
+++ src/lib/PhotoCard.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+</script>
+
+<div class="preview-card">
+  <div class="corner top left"></div>
+  <div class="corner top right"></div>
+  <div class="corner bottom left"></div>
+  <div class="corner bottom right"></div>
+  <p class="number">XVII</p>
+  <div class="photo-border">
+    <img class="photo" src="/the-hunt.jpg" />
+  </div>
+</div>
+
+<br />
+<br />
+<br />
+<br />
+<br />
+
+<div class="main-card">
+  <div class="corner top left"></div>
+  <div class="corner top right"></div>
+  <div class="corner bottom left"></div>
+  <div class="corner bottom right"></div>
+  <p class="number">XVII</p>
+  <div class="photo-border">
+    <img class="photo" src="/the-hunt.jpg" />
+  </div>
+  <div class="title-outline">
+    <div class="title-border">
+      <p class="title">The Hunt</p>
+    </div>
+  </div>
+</div>
+
+<br />
+<br />
+<br />
+<br />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import PhotoCard from "$lib/PhotoCard.svelte";
+</script>
+
+<PhotoCard />
