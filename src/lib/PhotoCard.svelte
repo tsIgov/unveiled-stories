5c8e6fc9e008dcfbe0c2fc7c785e46d73e3a2bab
Tsvetan Igov <ts.igov@outlook.com>
2025-03-27 01:15:19 +0200

    Added enhanced-img

--- src/lib/PhotoCard.svelte
+++ src/lib/PhotoCard.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 </script>
 
+
 <div class="preview-card">
   <div class="corner top left"></div>
   <div class="corner top right"></div>
@@ -8,7 +9,7 @@
   <div class="corner bottom right"></div>
   <p class="number">XVII</p>
   <div class="photo-border">
-    <img class="photo" src="/the-hunt.jpg" />
+    <enhanced:img class="photo" src="$lib/assets/the-hunt.jpg" alt="The Hunt" />
   </div>
 </div>
 
@@ -25,7 +26,7 @@
   <div class="corner bottom right"></div>
   <p class="number">XVII</p>
   <div class="photo-border">
-    <img class="photo" src="/the-hunt.jpg" />
+    <enhanced:img class="photo" src="$lib/assets/the-hunt.jpg" alt="The Hunt" />
   </div>
   <div class="title-outline">
     <div class="title-border">
