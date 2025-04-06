9d8c87a9e02117060aacafdaab6571a28c9e7442
Tsvetan Igov <ts.igov@outlook.com>
2025-04-07 02:30:51 +0300

    Improved layout

--- src/lib/components/CardSpread.svelte
+++ src/lib/components/CardSpread.svelte
@@ -2,46 +2,23 @@
 	import { t } from "$lib/utils";
 	import PhotoCard from "$lib/components/PhotoCard.svelte";
 
-	import heroimage from "$lib/assets/the-hunt-2.jpg?enhanced"
 	import image from "$lib/assets/the-hunt.jpg?enhanced";
 	import image2 from "$lib/assets/the-hunt-3.jpg?enhanced";
 	import image3 from "$lib/assets/the-hunt-4.jpg?enhanced";
+
+
+	let { ...others } :
+	{
+		class? : string | undefined,
+	} = $props();
 </script>
 
 
 
 
-<div class="w-full h-svh overflow-hidden">
-
-
-
-
-
-
-
-
-
-	<enhanced:img class="absolute object-cover w-full h-full saturate-[0.25] brightness-[0.25] opacity-20" src={heroimage} />
-
-	<div class="relative flex flex-col w-full h-full justify-center items-center p-16 gap-6">
-
-		<div class="relative shrink w-0 h-[calc((100vw_-_8rem)_*_1.6)]"> <!-- 8rem is the combined inline padding and 1.6 is the aspect ratio -->
-			<div class="-z-10 absolute h-full aspect-[0.625] -translate-x-[105%] translate-y-[18%] origin-top-left -rotate-15 scale-90"
-				style="box-shadow: rgba(100, 100, 100, 0.2) 0px 5px 25px 15px" > </div>
-			<PhotoCard class="absolute z-0 h-full -translate-x-[105%] translate-y-[18%] origin-top-left -rotate-15 scale-90 saturate-[0.30]" image={image3} />
-			<div class="-z-10 aspect-[0.625] absolute h-full -translate-x-[50%]"
-				style="box-shadow: rgba(100, 100, 100, 0.2) 0px 0px 15px 15px" > </div>
-			<PhotoCard class="absolute z-100 h-full -translate-x-[50%]" name={t({ en: "The Hunt", bg: "Ловът" })} number="XVIII" {image}	/>
-			<PhotoCard class="absolute z-0 h-full translate-x-[5%] translate-y-[18%] origin-top-right rotate-15 scale-90 saturate-[0.30]" image={image2} />
-			<div class="-z-10 aspect-[0.625] absolute h-full translate-x-[5%] translate-y-[18%] origin-top-right rotate-15 scale-90"
-				style="box-shadow: rgba(100, 100, 100, 0.2) 0px 0px 15px 15px" > </div>
-		</div>
-
-		<div class="w-sm max-w-full flex flex-col flex-none justify-center">
-			<p class="italic pb-8 text-[#ebead8] text-center">The hunt begins in silence, shadows stretching long beneath the ancient trees.</p>
-			<!-- <p class="italic pb-8 text-[#ebead8]">The hunt begins in silence, shadows stretching long beneath the ancient trees. The predator watches, eyes sharp, muscles tensed, as the whisper of the wind carries the scent of its prey. The stag moves cautiously, its hooves pressing into the damp earth, ears flicking at the distant rustle of unseen danger.</p> -->
-			<button class="border border-[#928f67]">See more</button>
-		</div>
+	<div class="relative items-start justify-center flex aspect-[1.26] {others.class}">
+		<PhotoCard class="left-1/2 absolute z-0 w-[48%] -translate-x-[105%] translate-y-[17%]  origin-top-left -rotate-15 scale-90 saturate-[0.30]" image={image3} />
+		<PhotoCard class="relative z-100 w-[48%] flex-none" name={t({ en: "The Hunt", bg: "Ловът" })} number="XVIII" {image}	/>
+		<PhotoCard class="right-1/2 top-0 absolute z-0 w-[48%] translate-x-[105%] translate-y-[17%] origin-top-right rotate-15 scale-90 saturate-[0.30]" image={image2} />
 	</div>
-</div>
 
--- src/routes/[[lang=lang]]/+page.svelte
+++ src/routes/[[lang=lang]]/+page.svelte
@@ -1,7 +1,24 @@
 <script lang="ts">
 	import CardSpread from "$lib/components/CardSpread.svelte";
+	import heroimage from "$lib/assets/the-hunt-2.jpg?enhanced"
+
 </script>
 
 
-<CardSpread />
+<div class="w-full h-svh overflow-hidden relative flex flex-col landscape:hsm:flex-row justify-center items-center p-16 gap-6">
+
+
+	<enhanced:img class="absolute left-0 top-0 object-cover w-full h-full saturate-[0.25] brightness-[0.25] opacity-20" src={heroimage} />
+
+	<!-- <CardSpread class="flex-auto h-0 max-w-[145svw] max-h-[115.08svw]" /> -->
+	<CardSpread class="flex-auto
+		h-0 max-w-[145svw] max-h-[115.08svw]
+		landscape:hsm:h-auto landscape:hsm:max-h-full  landscape:hsm:max-w-[85svh]" />
+
+	<div class="w-sm max-w-full">
+		<p class="italic w-full text-[#ebead8] text-center">The hunt begins in silence, shadows stretching long beneath the ancient trees.</p>
+		<!-- <p class="italic pb-8 text-[#ebead8]">The hunt begins in silence, shadows stretching long beneath the ancient trees. The predator watches, eyes sharp, muscles tensed, as the whisper of the wind carries the scent of its prey. The stag moves cautiously, its hooves pressing into the damp earth, ears flicking at the distant rustle of unseen danger.</p> -->
+		<button class="border w-full border-[#928f67]">See more</button>
+	</div>
+</div>
 
