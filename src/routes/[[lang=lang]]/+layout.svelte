e61dd6f4894dfc682d8a407ac8685f154e1272b5
Tsvetan Igov <ts.igov@outlook.com>
2025-06-10 17:14:06 +0300

    Fixed an issue with the opacity of the nav menu when refreshing not at the top of the page.

--- src/routes/[[lang=lang]]/+layout.svelte
+++ src/routes/[[lang=lang]]/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/state';
 	import NavMenu from '$lib/components/navigation/NavMenu.svelte';
+	import { onMount } from 'svelte';
 
 	let { children, data } = $props();
 
@@ -10,12 +11,19 @@
 	let navMenuMinOpacity = $derived(page.data.transparentNavigation ? 0 : 1);
 	let navMenuOpacity = $derived(Math.max(navMenuMinOpacity, Math.min(1, scrollY / navMenuOpacityThreshold)));
 
+	let scroller : HTMLElement;
+
+	onMount(() => {
+		scrollY = scroller.scrollTop;
+	});
+
 </script>
 
 <NavMenu currentLang={data.lang} currentRoute={data.route} opacity={navMenuOpacity} />
 
 <div class="w-full h-[100svh] oveflow-x-hidden overflow-y-scroll snap-y snap-proximity"
-	onscroll={(e) => scrollY = e.currentTarget.scrollTop} bind:offsetHeight={viewportHeight}>
+	onscroll={(e) => scrollY = e.currentTarget.scrollTop} bind:offsetHeight={viewportHeight}
+	bind:this={scroller}>
 	{@render children()}
 </div>
 
