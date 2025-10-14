e5ee571e411590978bf8ac307235f4c3a27bcdb5
Tsvetan Igov <ts.igov@outlook.com>
2025-10-14 19:08:40 +0300

    Optimization

--- src/components/cards/PhotoCard.svelte
+++ src/components/cards/PhotoCard.svelte
@@ -16,6 +16,8 @@
 
 	let { name, image, color, ...rest } : Props  = $props();
 
+	let el : HTMLElement;
+
 	let orientation = $derived(image.img.w > image.img.h ? "landscape" : "portrait");
 
 </script>
@@ -27,8 +29,9 @@
 	<Frame {color}>
 		<div class="frame-gap">
 			<Frame glow={false} {color}>
-				<enhanced:img class=""
-					src={image} alt={name} />
+				<img class="logo" src="/logo.svg" alt="logo" />
+				<enhanced:img class="" onload={() => { el.classList.add("loaded") }}
+					src={image} alt={name} bind:this={el} />
 			</Frame>
 		</div>
 	 </Frame>
@@ -49,10 +52,22 @@
 			padding: min(1.5cqb, 0.5rem);
 		}
 
+		& :global(.logo) {
+			@apply absolute w-full h-full top-0 left-0;
+			@apply object-fill -rotate-45;
+			@apply select-none;
+		}
+
 		& :global(img) {
 			@apply w-full h-full;
-			@apply bg-neutral-900 object-cover;
+			@apply min-w-4 min-h-4;
+			@apply object-cover;
 			@apply select-none;
+			@apply transition-opacity opacity-0 duration-1000;
+
+			&:global(.loaded) {
+				@apply opacity-100;
+			}
 		}
 	}
 </style>
