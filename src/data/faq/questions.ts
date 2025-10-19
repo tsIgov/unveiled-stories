2bde2c328b9e4e2a83077b550c20a6d7c49d9c37
Tsvetan Igov <ts.igov@outlook.com>
2025-10-19 19:14:37 +0300

    FAQ data

--- src/data/faq/questions.ts
+++ src/data/faq/questions.ts
@@ -13,77 +13,7 @@ export const questions : Question[] = [
 			bg: ""
 		},
 		answer: {
-			en: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-			bg: ""
-		}
-	},
-	{
-		question: {
-			en: "How many people can participate?",
-			bg: ""
-		},
-		answer: {
-			en: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-			bg: ""
-		}
-	},
-	{
-		question: {
-			en: "How much does it cost?",
-			bg: ""
-		},
-		answer: {
-			en: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-			bg: ""
-		}
-	},
-	{
-		question: {
-			en: "Can I bring a friend?",
-			bg: ""
-		},
-		answer: {
-			en: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-			bg: ""
-		}
-	},
-	{
-		question: {
-			en: "I don't know how to pose?",
-			bg: ""
-		},
-		answer: {
-			en: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-			bg: ""
-		}
-	},
-	{
-		question: {
-			en: "I'm not fit.",
-			bg: ""
-		},
-		answer: {
-			en: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-			bg: ""
-		}
-	},
-	{
-		question: {
-			en: "I don't have beautiful lingerie/clothes.",
-			bg: ""
-		},
-		answer: {
-			en: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-			bg: ""
-		}
-	},
-	{
-		question: {
-			en: "What if I don't live in Sofia?",
-			bg: ""
-		},
-		answer: {
-			en: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+			en: "Absolutely not. This is your story and you are in full control of what happens. We'll never push you to do anything you are not comfortable with.",
 			bg: ""
 		}
 	},
@@ -93,7 +23,147 @@ export const questions : Question[] = [
 			bg: ""
 		},
 		answer: {
-			en: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+			en: "It's up to you who sees the photos. We'll never share anything without your concent. We'll even send them to you via an encrypted channel.",
+			bg: ""
+		}
+	},
+	{
+		question: {
+			en: "Can I bring a friend?",
+			bg: ""
+		},
+		answer: {
+			en: "Of course. We welcome anyone and anything that will make you feel comfortable.",
+			bg: ""
+		}
+	},
+	{
+		question: {
+			en: "Who will be in the room during the shoot?",
+			bg: ""
+		},
+		answer: {
+			en: "You, the photographer, the photographer's assistant and whoever else you want.",
+			bg: ""
+		}
+	},
+	{
+		question: {
+			en: "How many people can participate?",
+			bg: ""
+		},
+		answer: {
+			en: "Boudoir doesn't need to be an individual experience. Your story may include spouces, friends, lovers or even some of our models. We welcome anything that will make the experience feel true to you.",
+			bg: ""
+		}
+	},
+	{
+		question: {
+			en: "How much does it cost?",
+			bg: ""
+		},
+		answer: {
+			en: "Our prices start at €270 and depend on the complexity of the story and the amount of preparation needed. We'll always be transparent and will give you a concrete offer before you commit to anything.",
+			bg: ""
+		}
+	},
+	{
+		question: {
+			en: "Do you offer any discounts?",
+			bg: ""
+		},
+		answer: {
+			en: "Yes, we offer up to 50% discount if you allow us to use some of the photos for our portfolio. Of course, you'll always have the final say in which photos we can use.",
+			bg: ""
+		}
+	},
+	{
+		question: {
+			en: "What will I get at the end?",
+			bg: ""
+		},
+		answer: {
+			en: "Except for the experience, you'll receive 20 to 200 (depending on the photoshoot) retouched pohotos carefully curated to tell your story. Additionally, you'll be able to order a leather album, a deck of cards or even an oil painting by our incredible artist.",
+			bg: ""
+		}
+	},
+	{
+		question: {
+			en: "I don't know how to pose.",
+			bg: ""
+		},
+		answer: {
+			en: "Don't worry, we'll guide you through the process but don't get too preoccupied by that. You don't need to perform, your job is to just have fun.",
+			bg: ""
+		}
+	},
+	{
+		question: {
+			en: "I'm not confident in my body.",
+			bg: ""
+		},
+		answer: {
+			en: "I started doing boudoir photography because my wife didn't find herself attractive. It turned out all she needed was to look at herself through the lens of someone admiring her beauty. We're here to be the lens that help you see youself in a new light.",
+			bg: ""
+		}
+	},
+	{
+		question: {
+			en: "I don't have beautiful lingerie/clothes.",
+			bg: ""
+		},
+		answer: {
+			en: "You don't have to. We'll find what best fits the story you want to tell and we'll go hunting. You can even book a session or go shopping with our amazing stylist.",
+			bg: ""
+		}
+	},
+	{
+		question: {
+			en: "Do you offer professional hair and makeup?",
+			bg: ""
+		},
+		answer: {
+			en: "Yes, we can set up an appointment with one of our hair dressers and makeup artists for the day of the shoot.",
+			bg: ""
+		}
+	},
+	{
+		question: {
+			en: "Where will the shoot take place?",
+			bg: ""
+		},
+		answer: {
+			en: "We'll scout for a private location at or near Sofia that fits the mood and style of your story.",
+			bg: ""
+		}
+	},
+	{
+		question: {
+			en: "What if I don't live in Sofia?",
+			bg: ""
+		},
+		answer: {
+			en: "We're open to travelling to most of Europe but we'll have to discuss the details on a case by case basis.",
+			bg: ""
+		}
+	},
+	{
+		question: {
+			en: "How far in advance should I book my session?",
+			bg: ""
+		},
+		answer: {
+			en: "Preparation may require creating storyboards, scouting for a location, buying or renting clothes and props, making appointments an so on. Be prepared that all of this might take up to a month but we'll discuss that in advance.",
+			bg: ""
+		}
+	},
+	{
+		question: {
+			en: "How long does the photoshoot take?",
+			bg: ""
+		},
+		answer: {
+			en: "We don't want to rush anything so it might take anything from a couple of hours to a whole day.",
 			bg: ""
 		}
 	},
