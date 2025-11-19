a832ce384d4302094630263d292fdbb9efa03078
Tsvetan Igov <ts.igov@outlook.com>
2025-11-19 15:42:36 +0200

    Optimization test

--- src/components/cards/PhotoCard.svelte
+++ src/components/cards/PhotoCard.svelte
@@ -20,7 +20,7 @@
 	<Frame {color}>
 		<div class="frame-gap">
 			<Frame glow={false} {color}>
-				<enhanced:img src={image.src} alt="" />
+				<!-- <enhanced:img src={image.src} alt="" /> -->
 			</Frame>
 		</div>
 	 </Frame>
