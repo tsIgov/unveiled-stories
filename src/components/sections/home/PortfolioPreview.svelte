ab1db50cd0da1f5303c472f374118966781d26c6
Tsvetan Igov <ts.igov@outlook.com>
2025-11-20 03:24:33 +0200

    Fixed the build

--- src/components/sections/home/PortfolioPreview.svelte
+++ src/components/sections/home/PortfolioPreview.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { Background, Ruler } from "components/basic";
 	import { Slideshow, Frame, LightboxGallery, PhotoCardSpread } from 'components/common';
-	import {  } from 'components/cards';
 
 	import { type Photoshoot, photoshoots, galleryEnabled } from 'data/photoshoots';
 	import { getTranslator } from '$lib/i18n/translator';
