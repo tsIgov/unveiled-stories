27ccfb7e6f79bd840ca554e0ca7e5478e9fa139d
Tsvetan Igov <ts.igov@outlook.com>
2025-11-20 23:32:56 +0200

    Brought back the glow

--- src/components/basic/Frame.svelte
+++ src/components/basic/Frame.svelte
@@ -21,7 +21,7 @@
 
 
 <div class="frame" style="--color: {color}">
-	{#if glow && (!$isTouchscreen || true)}
+	{#if glow && !$isTouchscreen}
 		<div transition:fade={{ duration: 1000, easing: cubicOut }} class="glow"></div>
 	{/if}
 	<div class="spinner cutout" class:chipped={chipped}></div>
