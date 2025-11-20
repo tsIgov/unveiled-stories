9737c7cecc628e1795ef4871c6d5a3594bffedcf
Tsvetan Igov <ts.igov@outlook.com>
2025-11-21 01:20:45 +0200

    Glow?

--- src/components/basic/Frame.svelte
+++ src/components/basic/Frame.svelte
@@ -21,7 +21,7 @@
 
 
 <div class="frame" style="--color: {color}">
-	{#if glow && !$isTouchscreen}
+	{#if glow && (true || !$isTouchscreen)}
 		<div transition:fade={{ duration: 1000, easing: cubicOut }} class="glow"></div>
 	{/if}
 	<div class="spinner cutout" class:chipped={chipped}></div>
