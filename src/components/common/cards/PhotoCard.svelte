2a482b1c07800bac601947a36ca7349f4565e9cf
Tsvetan Igov <ts.igov@outlook.com>
2025-11-20 19:47:44 +0200

    New attempt at asynchronous decoding

--- src/components/common/cards/PhotoCard.svelte
+++ src/components/common/cards/PhotoCard.svelte
@@ -21,7 +21,8 @@
 		<div class="frame-gap">
 			<Frame glow={false} {color}>
 				<enhanced:img src={image.src} alt=""  decoding="async" loading="eager" onload={(e) => {
-					// const img = e.target as HTMLImageElement;
+					const img = e.target as HTMLImageElement;
+					requestAnimationFrame(() => img.classList.add("initialized"));
 					// img.decode().finally(() => img.classList.add("initialized"))
 				}} />
 			</Frame>
