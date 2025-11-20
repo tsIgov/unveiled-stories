e4a991cfc4613e21e2595b548873c4693f2f212c
Tsvetan Igov <ts.igov@outlook.com>
2025-11-20 03:14:42 +0200

    Fixed an index

--- src/components/cards/index.ts
+++ src/components/cards/index.ts
@@ -1,4 +1,3 @@
 export { default as Card } from './Card.svelte'
-export { default as DetailsCard } from '../common/DetailsCard.svelte'
 export { default as PhotoCard } from './PhotoCard.svelte'
 export { default as PhotoCardSpread } from './PhotoCardSpread.svelte'
