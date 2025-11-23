a8465e293e96083d27b40230c2f1f06d527d6aed
Tsvetan Igov <ts.igov@outlook.com>
2025-11-23 22:00:02 +0200

    Slideshow now uses a timer

--- src/components/basic/Slider.svelte
+++ src/components/basic/Slider.svelte
@@ -8,14 +8,17 @@
 		itemsCount : number,
 		expandQuery?: string | undefined,
 		class?: ClassValue,
+		onchanged? : (index : number) => void,
+		autoChangeTime?: number
 		itemSnippet: Snippet<[index : number, distance: number]>
 	}
 
-	let { itemSnippet, itemsCount, loop = false, expandQuery = undefined, ...rest} : Props =$props();
+	let { itemSnippet, itemsCount, loop = false, expandQuery = undefined, onchanged, autoChangeTime = 0, ...rest} : Props =$props();
 
 
 	let initialized = $state(false);
 	let currentItem = $state(0);
+	let running = $state(false);
 
 	let observer : IntersectionObserver;
 	let track: HTMLElement;
@@ -24,6 +27,17 @@
 
 	export function changeItem(index : number) {
 		currentItem = index;
+		resetTimer();
+	}
+
+	export function pause() {
+		if (!running) return;
+		clearTimer();
+	}
+
+	export function unpause() {
+		if (running) return;
+		resetTimer();
 	}
 
 	onMount(() => {
@@ -40,10 +54,32 @@
 
 		resetScroll();
 		initialized = true;
+		resetTimer();
 
 		return () => {
+			clearTimer();
 			observer.disconnect();}
 	});
+
+	$effect(() => {
+		onchanged?.(currentItem);
+	});
+
+	let timer = -1;
+	function clearTimer() {
+		if (!autoChangeTime) return;
+		clearInterval(timer);
+		running = false;
+	}
+	function resetTimer() {
+		if (!autoChangeTime) return;
+
+		clearInterval(timer);
+		timer = setInterval(() => {
+			changeItem((currentItem + 1) % itemsCount);
+		}, autoChangeTime);
+		running = true;
+	}
 
 
 	function resetScroll(width : number = track.getBoundingClientRect().width) {
@@ -103,6 +139,27 @@
 		{#each { length: itemsCount }, index}
 			{@render itemSnippet(index, getDistance(index))}
 		{/each}
+
+		{#if autoChangeTime}
+			<div class="navigation">
+				{#each { length: itemsCount }, itemIndex}
+				<button
+						class:active={currentItem == itemIndex}
+						onclick="{() => changeItem(itemIndex)}"
+						tabindex="-1">
+						<span>{String(itemIndex + 1).padStart(2, '0')}</span>
+
+						<div
+							id="progress-line-{itemIndex}"
+							class="progress-line"
+							class:running={itemIndex == currentItem && running}
+							style="animation-duration: {autoChangeTime}ms">
+						</div>
+
+					</button>
+				{/each}
+			</div>
+		{/if}
 	</div>
 
 	<div bind:this={prevSpacer} class="spacer prev"></div>
@@ -136,6 +193,58 @@
 		@apply sticky left-0 w-full h-full shrink-0 overflow-x-hidden;
 
 		& > :global(*) { @apply absolute; }
+	}
+
+	.navigation {
+		@apply absolute p-4 w-full bottom-0 left-0 flex justify-center items-center gap-4;
+
+		& > button {
+			@apply flex justify-center items-center rounded-full cursor-pointer;
+			width: calc(var(--slideshow-navigation-height) - 2rem);
+			height: calc(var(--slideshow-navigation-height) - 2rem);
+
+			& > span {
+				@apply transition-colors duration-200 text-xs;
+				@apply text-neutral-100/60;
+
+			}
+
+			&:hover > span,
+			&.active > span {
+				@apply text-neutral-100;
+			}
+
+			& > .progress-line {
+				@apply transition-opacity duration-1000;
+				@apply absolute rounded-full bg-conic from-neutral-100/80 to-neutral-100/20 from-0% to-0%;
+				@apply opacity-0;
+
+				top: -2px;
+				left: -2px;
+				width: calc(var(--slideshow-navigation-height) - 2rem + 4px);
+				height: calc(var(--slideshow-navigation-height) - 2rem + 4px);
+				mask: radial-gradient(
+					transparent calc((var(--slideshow-navigation-height) - 2rem) / 2),
+					black calc((var(--slideshow-navigation-height) - 2rem) / 2 + 1px));
+
+				&.running {
+					@apply opacity-100;
+					animation-name: progress;
+					animation-timing-function: linear;
+				}
+			}
+		}
+	}
+
+	@keyframes progress {
+		from {
+			--tw-gradient-to-position: 0%;
+			--tw-gradient-from-position: 0%;
+		}
+		to {
+			--tw-gradient-to-position: 100%;
+			--tw-gradient-from-position: 100%;
+		}
 	}
 
 </style>
--- src/components/common/NewSlideshow.svelte
+++ src/components/common/NewSlideshow.svelte
@@ -1,184 +1,45 @@
 <script lang="ts" generics="T">
 	import { type Snippet, onMount } from "svelte";
+	import { Slider } from "components/basic"
 
 	interface Props {
 		data: T[],
 		slideSnippet: Snippet<[T, number]>
 		timeout: number,
-		onchanged? : (item : T, index : number) => void
+		onchanged? : (index : number) => void
 	}
 
 	let { data, slideSnippet, timeout, onchanged } : Props =$props();
 	let slidesCount = $derived(data.length);
 
-	export function pause() {
-		if (!running) return;
-		running = false;
-		clearTimer();
-	}
+	let slider : Slider;
 
-	export function unpause() {
-		if (running) return;
-
-		running = true;
-		resetTimer();
-	}
-
-	let initialized = $state(false);
-	let running = $state(false);
-	let currentSlide = $state(0);
-
-	let observer : IntersectionObserver;
-	let track: HTMLElement;
-	let prevSpacer : HTMLElement;
-	let nextSpacer : HTMLElement;
-
-
-	onMount(() => {
-		const options = {
-			root: track,
-			threshold: [ 0.501, 1 ]
-		};
-		observer =  new IntersectionObserver((e) => {
-			e.forEach(entry => { intersected(entry); });
-		}, options);
-
-		observer.observe(prevSpacer);
-		observer.observe(nextSpacer);
-
-		resetScroll();
-		initialized = true;
-		resetTimer();
-
-		return () => {
-			clearTimer();
-			observer.disconnect();}
-	});
-
-	$effect(() => {
-		onchanged?.(data[currentSlide], currentSlide);
-	});
-
-
-	function resetScroll(width : number = track.getBoundingClientRect().width) {
-		track.scrollTo({
-			left: width * 2,
-			behavior: "instant"
-		});
-	}
-
-	let timer = -1;
-	function clearTimer() {
-		clearInterval(timer);
-		running = false;
-	}
-	function resetTimer() {
-		clearInterval(timer);
-		timer = setInterval(() => {
-			changeSlide((currentSlide + 1) % slidesCount);
-		}, timeout);
-		running = true;
-	}
-
-
-
-	function changeSlide(index: number) {
-		currentSlide = index;
-		resetTimer();
-	}
-
-	function intersected(entry : IntersectionObserverEntry) {
-		if (!initialized) return;
-
-		if (entry.target == prevSpacer && entry.isIntersecting) {
-			if (entry.intersectionRatio == 1)
-				resetScroll(entry.boundingClientRect.width);
-			else {
-				currentSlide = (currentSlide - 1 + data.length) % slidesCount;
-				resetTimer();
-			}
-
-			return;
-		}
-
-		if (entry.target == nextSpacer && entry.isIntersecting) {
-			if (entry.intersectionRatio == 1)
-				resetScroll(entry.boundingClientRect.width);
-			else {
-				currentSlide = (currentSlide + 1) % slidesCount;
-				resetTimer();
-			}
-			return;
-		}
-	}
+	export function pause() { slider.pause(); }
+	export function unpause() {slider.unpause; }
 
 </script>
 
-<div bind:this={track} class="slideshow" class:initialized={initialized}>
-
-	<div class="slide-wrapper">
-		{#each data as slide, index}
-			<div class="slide" class:active={currentSlide == index}>
-				{@render slideSnippet(slide, index)}
-			</div>
-		{/each}
-
-		<div class="slideshow-nav">
-			{#each { length: slidesCount }, slideIndex}
-			<button
-					class:active={currentSlide == slideIndex}
-					onclick="{() => changeSlide(slideIndex)}"
-					tabindex="-1">
-					<span>{String(slideIndex + 1).padStart(2, '0')}</span>
-
-					<div
-						id="progress-line-{slideIndex}"
-						class="progress-line"
-						class:running={slideIndex == currentSlide && running}
-						style="animation-duration: {timeout}ms">
-					</div>
-
-				</button>
-			{/each}
+	{#snippet sliderSnippet(index : number, distance : number )}
+		<div class="slide" class:active={distance == 0}>
+			{@render slideSnippet(data[index], index)}
 		</div>
-	</div>
+	{/snippet}
 
-	<div bind:this={prevSpacer} class="spacer prev"></div>
-	<div class="spacer current"></div>
-	<div bind:this={nextSpacer} class="spacer next"></div>
-
-</div>
+<Slider
+	bind:this={slider}
+	class="slideshow"
+	itemsCount={slidesCount}
+	loop={true}
+	itemSnippet={sliderSnippet}
+	autoChangeTime={timeout}
+	{onchanged}
+/>
 
 <style>
 	@reference "style";
 
 	:global(:root) {
 		--slideshow-navigation-height: 3.5rem;
-	}
-
-	.slideshow {
-
-		@apply flex;
-
-		@apply overflow-x-scroll;
-		@apply snap-x snap-mandatory scroll-auto;
-
-		scrollbar-width: none;
-		-ms-overflow-style: none;
-	}
-
-	.slideshow:not(.initialized) {
-		& > .spacer.prev { @apply snap-none; }
-		& > .slide-wrapper > .slide:first-child { @apply opacity-100 visible; }
-	}
-
-	.spacer {
-		@apply w-full h-px shrink-0;
-		@apply snap-always snap-start;
-	}
-
-	.slide-wrapper {
-		@apply sticky left-0 w-full h-full shrink-0;
 	}
 
 	.slide {
@@ -192,58 +53,6 @@
 
 		& > :global(*) {
 			@apply w-full h-full;
-		}
-	}
-
-	.slideshow-nav {
-		@apply absolute p-4 w-full bottom-0 left-0 flex justify-center items-center gap-4;
-
-		& > button {
-			@apply flex justify-center items-center rounded-full cursor-pointer;
-			width: calc(var(--slideshow-navigation-height) - 2rem);
-			height: calc(var(--slideshow-navigation-height) - 2rem);
-
-			& > span {
-				@apply transition-colors duration-200 text-xs;
-				@apply text-neutral-100/60;
-
-			}
-
-			&:hover > span,
-			&.active > span {
-				@apply text-neutral-100;
-			}
-
-			& > .progress-line {
-				@apply transition-opacity duration-1000;
-				@apply absolute rounded-full bg-conic from-neutral-100/80 to-neutral-100/20 from-0% to-0%;
-				@apply opacity-0;
-
-				top: -2px;
-				left: -2px;
-				width: calc(var(--slideshow-navigation-height) - 2rem + 4px);
-				height: calc(var(--slideshow-navigation-height) - 2rem + 4px);
-				mask: radial-gradient(
-					transparent calc((var(--slideshow-navigation-height) - 2rem) / 2),
-					black calc((var(--slideshow-navigation-height) - 2rem) / 2 + 1px));
-
-				&.running {
-					@apply opacity-100;
-					animation-name: progress;
-					animation-timing-function: linear;
-				}
-			}
-		}
-	}
-
-	@keyframes progress {
-		from {
-			--tw-gradient-to-position: 0%;
-			--tw-gradient-from-position: 0%;
-		}
-		to {
-			--tw-gradient-to-position: 100%;
-			--tw-gradient-from-position: 100%;
 		}
 	}
 
