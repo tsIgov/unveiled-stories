1be5827bc21cef8c6d3fdffc179faa7e2e816910
Tsvetan Igov <ts.igov@outlook.com>
2025-11-24 00:36:47 +0200

    Small optimization

--- src/components/sections/home/Team.svelte
+++ src/components/sections/home/Team.svelte
@@ -3,8 +3,25 @@
 	import { Carousel, DetailsCard } from 'components/common';
 	import { getTranslator } from '$lib/i18n/translator';
 	import { type TeamMember, header, members } from 'data/team';
+	import { useMediaQuery } from "$lib/utils/media-queries";
 
 	let t = $derived(getTranslator());
+
+	let mediaQuery = getExpandMediaQuery();
+	let expanded = useMediaQuery(mediaQuery);
+
+	function getExpandMediaQuery() {
+		// Padding + item gaps + max item size
+		const remToFitLandscape = 1.5 * 2 + (members.length - 1) * 1.5 + members.length * 34;
+		const remToFitPortrait = 1.5 * 2 + (members.length - 1) * 1.5 + members.length * 22;
+
+		const landscapeMediaQuery = `(height < 40rem) and (orientation: landscape) and (width >= ${remToFitLandscape * 18}px)`;
+		const portraitMediaQuery = `((height >= 40rem) or (orientation: portrait)) and (width >= ${remToFitPortrait * 18}px)`;
+
+		const mediaQuery = `(${landscapeMediaQuery}) or (${portraitMediaQuery})`;
+
+		return mediaQuery;
+	}
 </script>
 
 {#snippet member(item : TeamMember, spotlight : boolean)}
@@ -19,10 +36,25 @@
 
 <section class="team">
 	<Heading title={t(header)} />
-	<Carousel itemSnippet={member} data={members} expandIfFit={true} />
+
+	{#if $expanded}
+		<div class="carousel expanded">
+			{#each members as m}
+				{@render member(m, true)}
+			{/each}
+		</div>
+	{:else}
+		<Carousel itemSnippet={member} data={members} />
+	{/if}
+
 </section>
 
 <style>
 	@reference "style";
 	.team :global(.card) { @apply w-full; }
+
+	.expanded {
+		@apply w-full flex gap-6;
+		@apply justify-center-safe items-center-safe content-center-safe;
+	}
 </style>
